e90309988bfb1cad0546a63e7da2207f711cdb86
mikearaya <mikaelaraya12@gmail.com>
2020-06-11 19:18:38 +0300

    Update conflicting bookmark test cases

--- tests/user-bookmarks.test.js
+++ tests/user-bookmarks.test.js
@@ -1,18 +1,14 @@
 import { setupDatabase, createLoggedInGraphqlFetch } from './helpers';
-import { SimpleProduct } from './seeds/products';
+import { UnpublishedProduct } from './seeds/products';
 import { ADMIN_TOKEN, User, Admin } from './seeds/users';
 
 let connection;
-let db;
 let graphqlFetch;
 
 describe('User Bookmarks', () => {
-  let Bookmarks;
-
   beforeAll(async () => {
-    [db, connection] = await setupDatabase();
+    [, connection] = await setupDatabase();
     graphqlFetch = await createLoggedInGraphqlFetch(ADMIN_TOKEN);
-    Bookmarks = db.collection('bookmarks');
   });
 
   afterAll(async () => {
@@ -34,59 +30,13 @@ describe('User Bookmarks', () => {
           }
         `,
         variables: {
-          productId: SimpleProduct._id,
+          productId: UnpublishedProduct._id,
           userId: User._id,
         },
       });
       expect(createBookmark).toMatchObject({
         user: {
           _id: User._id,
-        },
-      });
-    });
-  });
-
-  describe('Mutation.removeBookmark', () => {
-    it('remove a bookmark', async () => {
-      const bookmark = await Bookmarks.findOne({ userId: User._id });
-      const { data: { removeBookmark } = {} } = await graphqlFetch({
-        query: /* GraphQL */ `
-          mutation removeBookmark($bookmarkId: ID!) {
-            removeBookmark(bookmarkId: $bookmarkId) {
-              _id
-            }
-          }
-        `,
-        variables: {
-          bookmarkId: bookmark._id,
-        },
-      });
-      expect(removeBookmark._id).toBeTruthy();
-    });
-  });
-
-  describe('Mutation.bookmark', () => {
-    it('create a new bookmark for logged in user', async () => {
-      const { data: { bookmark } = {} } = await graphqlFetch({
-        query: /* GraphQL */ `
-          mutation bookmark($productId: ID!, $bookmarked: Boolean) {
-            bookmark(productId: $productId, bookmarked: $bookmarked) {
-              _id
-              created
-              user {
-                _id
-              }
-            }
-          }
-        `,
-        variables: {
-          productId: SimpleProduct._id,
-          bookmarked: true,
-        },
-      });
-      expect(bookmark).toMatchObject({
-        user: {
-          _id: Admin._id,
         },
       });
     });
@@ -111,13 +61,7 @@ describe('User Bookmarks', () => {
           userId: Admin._id,
         },
       });
-      expect(bookmarks).toMatchObject([
-        {
-          product: {
-            _id: SimpleProduct._id,
-          },
-        },
-      ]);
+      expect(bookmarks.length).toEqual(2);
     });
   });
 });
